c546319ee3a0e23b3e9fc8f875b17da1831c51dd
BossBoxing <boss_slam@hotmail.com>
2022-05-03 15:51:42 +0700

    Add Services , Message Component & Service

---
+++ src/app/hero.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { Hero } from '../app/heroes/hero';
+import { HEROES } from './mock-heroes';
+import { Observable, of } from 'rxjs';
+import { MessageService } from './message.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HeroService {
+
+
+  getHeroes(): Observable<Hero[]> {
+    const heroes = of(HEROES);
+    this.messageService.add('HeroService: fetched heroes');
+    return heroes;
+  }
+
+  constructor(private messageService: MessageService) { }
+}
